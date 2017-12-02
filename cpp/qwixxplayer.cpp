41f8f60d3967eb5116cb70c496fab89a55a0db7e
wesmartin17 <wes.martin17@gmail.com>
2017-12-01 21:33:43 -0500

    inputbeforeroll

---
+++ cpp/qwixxplayer.cpp
@@ -0,0 +1,57 @@
+#include "../h/qwixxplayer.h"
+
+void Player::inputBeforeRoll(RollOfDice &_rollOfDice){
+
+  QwixxPlayer *qp = dynamic_cast<QwixxPlayer*>(this); // can be used like this : qp->scoreSheet.score(~~~)
+
+  bool validSelection = false;
+  vector<string> selection;
+
+  while(!validSelection){
+      selection.clear();
+
+    cout<<"Please enter the color and column you would like to enter the white dice [placeholder whitedice] in.";
+    if(!active)
+      cout<<"(Or you can pass)"<<endl;
+    else
+      cout<<"(Or you can fail)"<<endl;
+
+    string input = "";
+    while (input != "done"){
+      cin>>input;
+      if(input == "pass" && !active){
+        validSelection = true;
+      }
+      else if (input == "fail" && active){
+        selection.push_back(input);
+        qp->scoreSheet.failedAttempts.push_back(_rollOfDice);
+        validSelection = true;
+      }
+
+    }
+
+    if(selection.size() >=3){
+      stringstream asdf(selection.end()[-2]);
+      int x = -1;
+      asdf >> x;
+
+      if(x > 0){
+        switch(selection.end()[-3]){
+          case("red"):
+            if(qp->scoreSheet.score(_rollOfDice,Dice::Color::red,x))
+              validSelection = true;
+              break;
+            /* ENTER IN THE REST!*/
+        }
+      }
+
+    }
+
+    if(!validSelection){
+      cout<<name<<", THAT WAS IN INVALID SELECTION!"<<endl<<endl;
+    }
+
+  }
+
+
+}
