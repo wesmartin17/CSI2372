def702b18c839269565146f38b985c4d2a88a1d7
KarimChukfeh <kchuk030@uottawa.ca>
2017-12-06 17:39:03 -0500

    put game's loops in seperate functions

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int versionSelection(){
 int playersNumberSelection(){
 
 	string input = "";
-	int numberPlayers = 0;
+	int numberOfPlayers = 0;
 
 	cout<<"Choose a number of players between 1 and 3" << endl;
 
@@ -43,13 +43,13 @@ int playersNumberSelection(){
 	   cout << "Your selection: ";
 	   getline(cin, input);
 	   stringstream myStream(input);
-	   if (myStream >> numberPlayers){
-			 if(numberPlayers >= 1 && numberPlayers <= 3)
+	   if (myStream >> numberOfPlayers){
+			 if(numberOfPlayers >= 1 && numberOfPlayers <= 3)
 	     break;
 		 }
 	   cout << "Invalid number, please try again" << endl;
 	 }
-	 return numberPlayers;
+	 return numberOfPlayers;
 }
 
 vector<QwintoPlayer> createQwintoPlayers(int numberOfPlayers){
@@ -94,6 +94,134 @@ vector<QwixxPlayer> createQwixxPlayers(int numberOfPlayers){
 	return players;
 }
 
+void playQwinto(int numberOfPlayers){
+	// Create Corresponding Players
+	vector<QwintoPlayer> players = createQwintoPlayers(numberOfPlayers);
+	bool gameOver = false;
+
+	//While end condition is not reached
+	while(!gameOver){
+		vector<QwintoPlayer>::iterator activePlayer; //Loop over players
+		for(activePlayer = players.begin(); activePlayer<players.end(); activePlayer++){
+
+			activePlayer->active = true; // next player takes a turn i.e., becomes active
+			cout << "\n" << activePlayer->name << " it's YOUR turn!!" << endl;
+			RollOfDice rd;
+			activePlayer->inputBeforeRoll(rd); // get input from active player before roll
+			rd.roll(); // roll the dice
+			cout << "\nRolling...\n .\\./.\\./.\\ ~~ [?]\n" << rd << " !!\n"; // show result
+			cout << "\nHere's what your sheet currently looks like: ";
+			cout << activePlayer->scoreSheet << endl; // print scoresheet of active player
+			cout << "Your rolled a [" << int(rd) << "]\n";
+			cout << "What would you like to do?\n" << endl;
+			activePlayer->inputAfterRoll(rd);  // get input from active player after roll
+			if(!activePlayer->scoreSheet){ // End condition met by active player
+				cout << "The game will end as a result." << endl;
+				gameOver = true;
+				break;
+			}
+
+			vector<QwintoPlayer>::iterator inactivePlayer;
+			for(inactivePlayer = players.begin(); inactivePlayer<players.end(); inactivePlayer++){
+				if(!inactivePlayer->active){
+					cout << inactivePlayer->scoreSheet << endl;
+					inactivePlayer->inputAfterRoll(rd);
+					if(!inactivePlayer->scoreSheet){ // End condition met by inactive player
+						cout << "The game will end as a result." << endl;
+						gameOver = true;
+						break;
+					}
+				}
+			}
+			activePlayer->active = false; // mark player as inactive and go to next player
+		}
+	}
+	/*
+		GAME IS OVER
+	*/
+	QwintoPlayer winner = players[0]; // placeholder value
+	string winnerName = winner.name; // placeholder value
+	int winnerScore = -100; // placeholder value
+	int score = winnerScore; // placeholder value
+
+	for(int i = 0; i <numberOfPlayers; ++i){
+		score = players[i].scoreSheet.setTotal(); // Calculate score for each player
+		cout << players[i].scoreSheet; // Show score sheet for each player
+		if(winnerScore < score){
+			winnerScore = score;
+			winner = players[i];
+			winnerName = players[i].name;
+		}
+	}
+	// Announce winner.
+	cout << "Congradulations to " << winnerName << " for winning the game with " << winnerScore << " points!" << endl;
+	return;
+}
+
+void playQwixx(int numberOfPlayers){
+	vector<QwixxPlayer> players = createQwixxPlayers(numberOfPlayers);
+	bool gameOver = false;
+
+	//While end condition is not reached
+	while(!gameOver){
+		vector<QwixxPlayer>::iterator activePlayer; //Loop over players
+		for(activePlayer = players.begin(); activePlayer<players.end(); activePlayer++){
+
+			activePlayer->active = true; // next player takes a turn i.e., becomes active
+			cout << "\n" << activePlayer->name << " it's YOUR turn!!" << endl;
+			RollOfDice rd;
+			rd.dices.push_back(Dice(Color::white));
+			rd.dices.push_back(Dice(Color::white));
+			rd.dices.push_back(Dice(Color::red));
+			rd.dices.push_back(Dice(Color::yellow));
+			rd.dices.push_back(Dice(Color::green));
+			rd.dices.push_back(Dice(Color::blue));
+			RollOfDice wd = RollOfDice::pair(rd.dices[0], rd.dices[1]);
+
+			rd.roll();
+			cout << "\nRolling dice...\n .\\./.\\./.\\./.\\./.\\./ ~~ [?][?][?][?][?][?]\n";
+			cout << rd;
+			cout << "----------------------------------"<<endl;
+			cout << "WHITE #1: ["<<rd.dices[0].face<<"]"<<endl;
+			cout << "WHITE #2: ["<<rd.dices[1].face<<"]"<<endl<<endl;
+			cout << "SUM: "<<int(wd)<<endl;
+			cout << "----------------------------------"<<endl;
+			cout<<activePlayer->scoreSheet<<endl;
+			activePlayer->inputBeforeRoll(wd);
+			for(vector<QwixxPlayer>::iterator i = players.begin(); i != players.end(); ++i){
+				if(!i->active){
+					cout<<i->scoreSheet<<endl;
+					i->inputBeforeRoll(wd);
+				}
+
+			}
+
+			cout << "\n" << activePlayer->name
+					<< ", it's you turn to choose a white dice (number 1 or number 2) as well as a colored dice to score their sum." << endl << activePlayer->scoreSheet <<endl<<rd <<endl;
+			activePlayer->inputAfterRoll(rd);
+
+			if(!activePlayer->scoreSheet){ // End condition met by active player
+				cout << "The game will end as a result." << endl;
+				gameOver = true;
+				break;
+			}
+
+			vector<QwixxPlayer>::iterator inactivePlayer;
+			for(inactivePlayer = players.begin(); inactivePlayer<players.end(); inactivePlayer++){
+				if(!inactivePlayer->active){
+					if(!inactivePlayer->scoreSheet){ // End condition met by inactive player
+						cout << "The game will end as a result." << endl;
+						gameOver = true;
+						break;
+					}
+				}
+			}
+			activePlayer->active = false; // mark player as inactive and go to next player
+		}
+	}
+	return;
+}
+
 int main() {
 
 	/*
@@ -104,174 +232,14 @@ int main() {
 	int numberOfPlayers = playersNumberSelection(); // Ask player to choose number of players
 
 	if(gameVersion==1){ // PLAYING QWINTO
-
-		// Create Corresponding Players
-		vector<QwintoPlayer> players = createQwintoPlayers(numberOfPlayers);
-
-		/*
-			While end condition is not reached
-		*/
-		bool gameOver = false;
-		while(!gameOver){
-
-
-			vector<QwintoPlayer>::iterator activePlayer; //Loop over qwinto players
-			for(activePlayer = players.begin(); activePlayer<players.end(); activePlayer++){
-
-				activePlayer->active = true; // next player takes a turn i.e., becomes active
-				cout << "\n" << activePlayer->name << " it's YOUR turn!!" << endl;
-				RollOfDice rd;
-				activePlayer->inputBeforeRoll(rd); // get input from active player before roll
-				rd.roll(); // roll the dice
-				cout << "\nRolling...\n .\\./.\\./.\\ ~~ [?]\n" << rd << " !!\n"; // show result
-				cout << "\nHere's what your sheet currently looks like: ";
-				cout << activePlayer->scoreSheet << endl; // print scoresheet of active player
-				cout << "Your rolled a [" << int(rd) << "]\n";
-				cout << "What would you like to do?\n" << endl;
-				activePlayer->inputAfterRoll(rd);  // get input from active player after roll
-				if(!activePlayer->scoreSheet){ // End condition met by active player
-					cout << "The game will end as a result." << endl;
-					gameOver = true;
-					break;
-				}
-
-				vector<QwintoPlayer>::iterator inactivePlayer;
-				for(inactivePlayer = players.begin(); inactivePlayer<players.end(); inactivePlayer++){
-					if(!inactivePlayer->active){
-						cout << inactivePlayer->scoreSheet << endl;
-						inactivePlayer->inputAfterRoll(rd);
-						if(!inactivePlayer->scoreSheet){ // End condition met by inactive player
-							cout << "The game will end as a result." << endl;
-							gameOver = true;
-							break;
-						}
-					}
-				}
-				activePlayer->active = false; // mark player as inactive and go to next player
-			}
-		}
-		/*
-			GAME IS OVER
-		*/
-		QwintoPlayer winner = players[0]; // placeholder value
-		string winnerName = winner.name; // placeholder value
-		int winnerScore = -100; // placeholder value
-		int score = winnerScore; // placeholder value
-
-		for(int i = 0; i <numberOfPlayers; ++i){
-			score = players[i].scoreSheet.setTotal(); // Calculate score for each player
-			cout << players[i].scoreSheet; // Show score sheet for each player
-			if(winnerScore < score){
-				winnerScore = score;
-				winner = players[i];
-				winnerName = players[i].name;
-			}
-		}
-		// Announce winner.
-		cout << "Congradulations to " << winnerName << " for winning the game with " << winnerScore << " points!" << endl;
-
-	}else{//PLAYING QWIXX
-			vector<QwixxPlayer> players = createQwixxPlayers(numberOfPlayers);
-
-			bool gameOver = false;
-			while(!gameOver){
-							vector<QwixxPlayer>::iterator activePlayer; //Loop over qwinto players
-							for(activePlayer = players.begin(); activePlayer<players.end(); activePlayer++){
-
-								activePlayer->active = true; // next player takes a turn i.e., becomes active
-								cout << "\n" << activePlayer->name << " it's YOUR turn!!" << endl;
-								RollOfDice rd;
-								rd.dices.push_back(Dice(Color::white));
-								rd.dices.push_back(Dice(Color::white));
-								rd.dices.push_back(Dice(Color::red));
-								rd.dices.push_back(Dice(Color::yellow));
-								rd.dices.push_back(Dice(Color::green));
-								rd.dices.push_back(Dice(Color::blue));
-								RollOfDice wd = RollOfDice::pair(rd.dices[0], rd.dices[1]);
-
-								rd.roll();
-								cout << "\nRolling dice...\n .\\./.\\./.\\./.\\./.\\./ ~~ [?][?][?][?][?][?]\n";
-								cout << rd;
-								cout << "----------------------------------"<<endl;
-								cout << "WHITE #1: ["<<rd.dices[0].face<<"]"<<endl;
-								cout << "WHITE #2: ["<<rd.dices[1].face<<"]"<<endl<<endl;
-								cout << "SUM: "<<int(wd)<<endl;
-								cout << "----------------------------------"<<endl;
-								cout<<activePlayer->scoreSheet<<endl;
-								activePlayer->inputBeforeRoll(wd);
-								for(vector<QwixxPlayer>::iterator i = players.begin(); i != players.end(); ++i){
-									if(!i->active){
-										cout<<i->scoreSheet<<endl;
-										i->inputBeforeRoll(wd);
-									}
-
-								}
-
-								cout << "\n" << activePlayer->name
-										<< ", it's you turn to choose a white dice (number 1 or number 2) as well as a colored dice to score their sum." << endl << activePlayer->scoreSheet <<endl<<rd <<endl;
-								activePlayer->inputAfterRoll(rd);
-
-								if(!activePlayer->scoreSheet){ // End condition met by active player
-									cout << "The game will end as a result." << endl;
-									gameOver = true;
-									break;
-								}
-
-								vector<QwixxPlayer>::iterator inactivePlayer;
-								for(inactivePlayer = players.begin(); inactivePlayer<players.end(); inactivePlayer++){
-									if(!inactivePlayer->active){
-										if(!inactivePlayer->scoreSheet){ // End condition met by inactive player
-											cout << "The game will end as a result." << endl;
-											gameOver = true;
-											break;
-										}
-									}
-								}
-								activePlayer->active = false; // mark player as inactive and go to next player
-							}
-				}
+		playQwinto(numberOfPlayers);
+	}
+	if(gameVersion==2){ // PLAYING QWIXX
+		playQwixx(numberOfPlayers);
 	}
 
 	/*
 		end of main routine area
 	*/
-
-
-	// How to use QwixxRow += rd
-
-
-	// QwixxScoreSheet kc = QwixxScoreSheet("karim");
-  //
-	// RollOfDice rd1 = RollOfDice::pair(Dice(Color::blue), Dice(Color::white));
-	// try {// this is ok because 1x blue dice
-	// 	kc.blueRow += rd1;
-	// }
-	// catch( const std::invalid_argument& e ) {
-	//     cout << e.what() << endl;
-	// }
-  //
-	// RollOfDice rd2 = RollOfDice::pair(Dice(Color::white), Dice(Color::white));
-	// try { // this is ok because 2x white dice
-	// 	kc.blueRow += rd2;
-	// }
-	// catch( const std::invalid_argument& e ) {
-	// 		cout << e.what() << endl;
-	// }
-  //
-	// RollOfDice rd3 = RollOfDice::pair(Dice(Color::red), Dice(Color::white));
-	// try { // this is not ok because no 1x blue dice and no 2x white dice
-	// 	kc.blueRow += rd3;
-	// }
-	// catch( const std::invalid_argument& e ) {
-	//     cout << e.what() << endl;
-	// }
-  //
-	// RollOfDice rd4 = RollOfDice::pair(Dice(Color::blue), Dice(Color::blue));
-	// try {  // this is not ok because the combo doesn't have at least 1x white dice
-	// 	kc.blueRow += rd4;
-	// }
-	// catch( const std::invalid_argument& e ) {
-	// 		cout << e.what() << endl;
-	// }
 
 }
